a91c3c98bfc53016acb7194e4ce8b4e188b9a01d
gutaewoo <gutaewoo@naver.com>
2016-08-13 00:54:53 +0900

    Update Core.Logic.cs

--- Nechrito Riven/Core/Core.Logic.cs
+++ Nechrito Riven/Core/Core.Logic.cs
@@ -16,7 +16,7 @@ namespace NechritoRiven.Core
         private static bool forceR;
         private static bool forceR2;
         private static bool forceItem;
-         public static float lastQ;
+         public static float LastQ;
         
         private static int Item
             =>
