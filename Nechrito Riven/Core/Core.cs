5a1d43ea1e0ebaa7f6653b3ff3745b455bc94e4a
gutaewoo <gutaewoo@naver.com>
2016-08-14 07:07:09 +0900

    Update Core.cs

--- Nechrito Riven/Core/Core.cs
+++ Nechrito Riven/Core/Core.cs
@@ -14,6 +14,5 @@ namespace NechritoRiven.Core
 
         public static Orbwalking.Orbwalker _orbwalker;
         public static Obj_AI_Hero Player => ObjectManager.Player;
-        public static Obj_AI_Hero Target => TargetSelector.GetTarget(250 + Player.AttackRange + 70, TargetSelector.DamageType.Physical);
     }
 }
